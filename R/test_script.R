7551d740f94eb208790c70a926e457b562d87b68
philliplab <philliplab@gmail.com>
2014-09-16 10:35:33 +0200

    Package Skeleton

---
+++ R/test_script.R
@@ -0,0 +1,7 @@
+# Script to get the package skeleton going
+
+#' Prints Hello
+#' @export
+hello <- function(){
+  print('Hello')
+}
---
+++ tests/testthat/test-testing.R
@@ -0,0 +1,5 @@
+context('Check testing')
+
+test_that('testing code runs',{
+  expect_that(1+1, equals(2))
+})
