fa6b0198a699af7b27fe0936140780c7220f8efd
philliplab <philliplab@gmail.com>
2014-09-16 14:11:11 +0200

    add lanl_hla_data class

---
+++ R/lanl_hla_data.R
@@ -0,0 +1,42 @@
+#' The class for the data from LANL that describe the HLA genotype's epitopes
+#' 
+#' @rdname LANL_HLA_data
+#' @aliases LANL_HLA_data-class
+#' @exportClass LANL_HLA_data
+#' @export .LANL_HLA_data
+
+.LANL_HLA_data <- setClass(
+  Class = 'LANL_HLA_data',
+  representation = representation(),
+  contains = 'data.frame',
+
+  validity = function(object){
+    col_names <- c("epitope", "gene_name", "start_pos", "end_pos", 
+                   "sub_type", "organism", "hla_genotype")
+    for (i in 1:length(col_names)){
+      if (names(object)[i] != col_names[i]){
+        stop(paste0("incorrent column names. ", i, "th column must be ", col_names[i]))
+      }
+    }
+    if (nrow(object) == 0){
+      stop("Must describe at least HLA genotype")
+    }
+  }
+)
+
+#' A function that reads a patient HLA genotype specification file
+#' @param file_name Name of the file
+#' @export
+
+read_lanl_hla <- function(file_name = NULL){
+  if (is.null(file_name)){
+    warning("No file name supplied to read_patient_hla - using test data")
+    file_name <- file.path(find.package('EpitopeMatcher', .libPaths()), 
+                           'test_data/lanl_hla_file.csv')
+  }
+  x <- read.csv(file_name,
+                stringsAsFactors = FALSE)
+  return(.LANL_HLA_data(x))
+}
+
+
---
+++ tests/testthat/test-lanl_hla.R
@@ -0,0 +1,24 @@
+context("LANL_HLA")
+
+test_that("LANL_HLA_data Class Constructor works", {
+  x <- data.frame(dude = 'CAP117', mutation = 'B*3501')
+  expect_that(.LANL_HLA_data(x), throws_error("incorrent column names"))
+
+  x <- structure(list(epitope = c("MGARASVLSG", "ASVLSGGEL", "ASILRGGKLDK"
+  ), gene_name = c("Gag_p17", "Gag_p17", "Gag_p17"), start_pos = c(1L, 
+  5L, 5L), end_pos = c(10L, 13L, 15L), sub_type = c("CRF01_AE", 
+  "B", "C"), organism = c("human", "human", "human"), hla_genotype = c("", 
+  "", "")), .Names = c("epitope", "gene_name", "start_pos", "end_pos", 
+  "sub_type", "organism", "hla_genotype"), row.names = c(NA, 3L
+  ), class = "data.frame")
+
+  expect_that(.LANL_HLA_data(x[0,]), throws_error("Must describe at least HLA genotype"))
+  expect_that(.LANL_HLA_data(x), is_a('LANL_HLA_data'))
+})
+
+test_that("read_patient_hla works", {
+  x <- read_lanl_hla(file.path(find.package('EpitopeMatcher', .libPaths()), 
+                                  'test_data/lanl_hla_file.csv'))
+  expect_that(x, is_a('LANL_HLA_data'))
+  expect_that(digest(x), equals("b127da9014e60c9ad973880e0d6ecc99"))
+})
