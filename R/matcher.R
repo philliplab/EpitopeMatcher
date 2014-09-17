0c8973e1a4d7db62c1852c3af8969ccdf6c99b87
philliplab <philliplab@gmail.com>
2014-09-17 10:19:15 +0200

    add draft sequence comparison function

--- R/matcher.R
+++ R/matcher.R
@@ -55,3 +55,71 @@ list_epitopes <- function(query_alignment, patient_hla, lanl_hla_data){
   return(lanl_hla_data[lanl_hla_data$hla_genotype %in% hlas, 
                        c('epitope', 'start_pos', 'end_pos')])
 }
+
+#' Finds the position of the epitope in the reference sequence
+#'
+#' It uses pairwiseAlignment with the default settings. See Biostrings manual
+#'
+#' Lots of things to test and investigate that can improve this function.
+#' @param epitope The epitope to find in the sequence. Either a character
+#' string or an AAString
+#' @param query_alignment The query alignment
+#' @export
+
+epitope_pos_in_ref <- function(epitope, query_alignment){
+  if (class(epitope) == 'character'){
+    epitope <- AAString(epitope)
+  }
+
+  # There must be a better way to do this - couldn't find it in Biostrings ref
+  # man
+  ref_seq <- as.character(query_alignment)[1]
+  ref_seq <- AAString(ref_seq)
+
+  alignment <- pairwiseAlignment(pattern = epitope, subject = ref_seq, type = 'overlap')
+  start_pos <- start(Views(alignment))
+  end_pos <- end(Views(alignment))
+  return(list(start_pos = start_pos,
+              end_pos = end_pos, 
+              alignment = alignment))
+}
+
+.sequence_comparison_stats <- function(sequence_substr, pair_alignments, start_pos, end_pos){
+  results <- NULL
+  for (i in 1:length(pair_alignments)){
+    x <- data.frame(name = names(sequence_substr)[i],
+                    score = score(pair_alignments[i]),
+                    score_type = type(pair_alignments[i]),
+                    epitope = as.character(subject(pair_alignments[i])),
+                    sub_sequence = as.character(pattern(pair_alignments[i])),
+                    comparison = compareStrings(pair_alignments[i]),
+                    pid = pid(pair_alignments[i]),
+                    nmatch = nmatch(pair_alignments[i]),
+                    nmismatch = nmismatch(pair_alignments[i]),
+                    leven.dist = nedit(pair_alignments[i]),
+                    start_pos = start_pos,
+                    end_pos = end_pos,
+                    stringsAsFactors = FALSE)
+    results <- rbind(results, x)
+  }
+  return(results)
+}
+
+#' Computes the similarities between the epitope and the sequences in the
+#' alignment
+#' @param epitope The epitope to find in the sequence. Either a character
+#' string or an AAString
+#' @param query_alignment The query alignment
+#' @export
+
+compute_epitope_scores <- function(epitope, query_alignment){
+  if (class(epitope) == 'character'){
+    epitope <- AAString(epitope)
+  }
+  ref_pos <- epitope_pos_in_ref(epitope, query_alignment)
+  sequence_substr <- substr(qa, ref_pos$start_pos, ref_pos$end_pos)
+  sequence_substr <- lapply(sequence_substr, AAString)
+  pair_alignments <- pairwiseAlignment(pattern = sequence_substr, subject = epitope)
+  results <- .sequence_comparison_stats(sequence_substr, pair_alignments, ref_pos$start_pos, ref_pos$end_pos)
+  return(results)
+}
