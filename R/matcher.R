2a46ba7c91ae55a08939c47f5fc3ee6b70b9a9e5
philliplab <philliplab@gmail.com>
2014-09-17 07:46:07 +0200

    add list_hlas function

--- R/matcher.R
+++ R/matcher.R
@@ -12,9 +12,20 @@ get_matchable_patient_ids <- function(query_alignment, patient_hla){
 
   qa_ids <- get_patient_ids(query_alignment)
   ph_ids <- get_patient_ids(patient_hla)
-  both_ids <- qa_ids[qa_ids %in% ph_ids]
-  if (length(unique(qa_ids)) != length(unique(both_ids))){
+  m_ids <- qa_ids[qa_ids %in% ph_ids]
+  if (length(unique(qa_ids)) != length(unique(m_ids))){
     warning("Not all patients in query_alignment have hla genotypes specified.
             They will not be analyzed") }
-  return(both_ids)
+  return(m_ids)
+}
+
+#' Lists all the hla genotypes that must be investigated
+#' @param query_alignment The query alignment
+#' @param patient_hla The data.frame (of class Patient_HLA) that contain lists
+#' all the HLA genotypes each patient has.
+#' @export
+
+list_hlas <- function(query_alignment, patient_hla){
+  m_ids <- get_matchable_patient_ids(query_alignment, patient_hla)
+  return(patient_hla[patient_hla$patient_id %in% m_ids, 'hla_genotype'])
 }
--- tests/testthat/test-matcher.R
+++ tests/testthat/test-matcher.R
@@ -9,3 +9,19 @@ test_that('get_matchable_patient_ids works', {
   expect_that(sort(m_ids), equals(c("P00885", "P01288", "P02869", "P03257", "V05130", "V05649", 
     "V11204", "V11474")))
 })
+
+test_that('list_hlas works', {
+  input_hlas <- c("A*0123", "A*0201", "A*0205", "A*0301", "A*2401", "A*2901", 
+                  "A*3001", "A*3301", "A*3402", "A*4301", "A*6601", "A*6601", "A*6601", 
+                  "A*6801", "B*1503", "B*1503", "B*1510", "B*1516", "B*1801", "B*3910", 
+                  "B*4201", "B*4201", "B*4403", "B*4403", "B*4403", "B*4501", "B*5101", 
+                  "B*5702", "B*5802", "Cw*0210", "Cw*0304", "Cw*0401", "Cw*0401", 
+                  "Cw*0602", "Cw*0701", "Cw*0704", "Cw*0704", "Cw*1203", "Cw*1601", 
+                  "Cw*1601", "Cw*1601", "Cw*1701", "Cw*1701", "Cw*1801")
+  qa <- read_query_alignment(file.path(find.package('EpitopeMatcher', .libPaths()), 
+                                  'test_data/query_alignment.FASTA'))
+  ph <- read_patient_hla(file.path(find.package('EpitopeMatcher', .libPaths()), 
+                                  'test_data/patient_hla_file.csv'))
+  hlas_to_target <- list_hlas(qa, ph)
+  expect_that(sort(hlas_to_target), equals(input_hlas))
+})
