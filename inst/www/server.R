50bd0cfb49af14cabc3b1afe83cae296b8ae7a0e
philliplab <philliplab@gmail.com>
2014-09-23 10:15:19 +0200

    add all file uploads and print them in shiny

--- inst/www/server.R
+++ inst/www/server.R
@@ -3,6 +3,10 @@ library(EpitopeMatcher)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  output$file_details <- renderPrint(print(input$patient_hla$datapath))
+  output$patient_hla_file_details <- renderPrint(print(input$patient_hla$datapath))
+  output$lanl_hla_file_details <- renderPrint(print(input$lanl_hla$datapath))
+  output$query_alignment_file_details <- renderPrint(print(input$query_alignment$datapath))
   output$patient_hla <- renderTable(read_patient_hla(input$patient_hla$datapath))
+  output$lanl_hla <- renderTable(read_lanl_hla(input$lanl_hla$datapath))
+  output$query_alignment <- renderPrint(print(read_query_alignment(input$query_alignment$datapath)))
 })
--- inst/www/ui.R
+++ inst/www/ui.R
@@ -9,13 +9,23 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
-      fileInput("patient_hla", label = h3("Patient HLA genotypes"))
+      fileInput("patient_hla", label = h3("Patient HLA genotypes")),
+      fileInput("lanl_hla", label = h3("LANL HLA genotype database")),
+      fileInput("query_alignment", label = h3("Query Alignment"))
     ),
 
     # Show a plot of the generated distribution
     mainPanel(
-      textOutput("file_details"),
-      tableOutput("patient_hla")
+      tabsetPanel(
+        tabPanel("File Details",
+                 textOutput("patient_hla_file_details"),
+                 textOutput("lanl_hla_file_details"),
+                 textOutput("query_alignment_file_details")
+                 ),
+        tabPanel("Patient HLA File", tableOutput("patient_hla")),
+        tabPanel("LANL HLA File", tableOutput("lanl_hla")),
+        tabPanel("Query Alignment", verbatimTextOutput("query_alignment"))
+        )
     )
   )
 ))
