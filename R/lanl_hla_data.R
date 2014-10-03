b12e2acdaf87d7bedbb43ee31cddcd87bc2d3377
philliplab <philliplab@gmail.com>
2014-10-03 09:20:46 +0200

    expand read_lanl_hla_file to perform so column name sanitization

--- R/lanl_hla_data.R
+++ R/lanl_hla_data.R
@@ -12,7 +12,7 @@
 
   validity = function(object){
     col_names <- c("epitope", "gene_name", "start_pos", "end_pos", 
-                   "sub_type", "organism", "hla_genotype")
+                   "subprotein", "hxb2_dna_position", "sub_type", "organism", "hla_genotype")
     for (i in 1:length(col_names)){
       if (names(object)[i] != col_names[i]){
         stop(paste0("incorrent column names. ", i, "th column must be ", col_names[i]))
@@ -29,13 +29,41 @@
 #' @export
 
 read_lanl_hla <- function(file_name){
-#  if (is.null(file_name)){
-#    warning("No file name supplied to read_patient_hla - using test data")
-#    file_name <- file.path(find.package('EpitopeMatcher', .libPaths()), 
-#                           'test_data/lanl_hla_file.csv')
-#  }
   x <- read.csv(file_name,
                 stringsAsFactors = FALSE)
+  column_renames <- list('1' = list(source = 'Epitope',
+                                    target = 'epitope',
+                                    col_num = 1),
+                         '2' = list(source = 'Protein',
+                                    target = 'gene_name',
+                                    col_num = 2),
+                         '3' = list(source = 'HXB2.start',
+                                    target = 'start_pos',
+                                    col_num = 3),
+                         '4' = list(source = 'HXB2.end',
+                                    target = 'end_pos',
+                                    col_num = 4),
+                         '5' = list(source = 'Subprotein',
+                                    target = 'subprotein',
+                                    col_num = 5),
+                         '6' = list(source = 'HXB2.DNA.Contig',
+                                    target = "hxb2_dna_position",
+                                    col_num = 6),
+                         '7' = list(source = 'Subtype',
+                                    target = 'sub_type',
+                                    col_num = 7),
+                         '8' = list(source = 'Species',
+                                    target = 'organism',
+                                    col_num = 8),
+                         '9' = list(source = 'HLA',
+                                    target = 'hla_genotype',
+                                    col_num = 9)
+                                    )
+  for (i in seq_along(column_renames)){
+    if (names(x)[column_renames[[i]]$col_num] == column_renames[[i]]$source){
+      names(x)[column_renames[[i]]$col_num] <- column_renames[[i]]$target
+    }
+  }
   return(.LANL_HLA_data(x))
 }
 
