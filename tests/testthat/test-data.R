4fff7cba87dc5f55277b1a910d7dbd6a818084d5
philliplab <philliplab@gmail.com>
2014-10-03 09:47:01 +0200

    fix unit tests for data sets

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,15 +2,15 @@ context('Data Files')
 
 test_that('the correct test data files are supplied', {
   x <- read.csv(file.path(find.package('EpitopeMatcher', .libPaths()), 
-                          'test_data/patient_hla_file.csv'))
-  expect_that(digest(x), equals("549bc9e44c1bb5412f493f57832a7ee5"))
+                          'test_data/patient_hla_test_file.csv'))
+  expect_that(digest(x), equals("4059bf3fee7ff3c9a982c59f620b70f0"))
 
   x <- read.csv(file.path(find.package('EpitopeMatcher', .libPaths()), 
-                          'test_data/lanl_hla_file.csv'))
-  expect_that(digest(x), equals("467f8f2140afdd3cbc8043b28d6e2245"))
+                          'test_data/lanl_hla_test_file.csv'))
+  expect_that(digest(x), equals("58fea75008a554f4f95fb16309b79fb9"))
 
   x <- readAAMultipleAlignment(file.path(find.package('EpitopeMatcher', .libPaths()), 
-                          'test_data/query_alignment.FASTA'))
-  expect_that(digest(x), equals("b6a4178dc682837e8cbd656a4395a8b2"))
+                          'test_data/query_alignment_test_file.fasta'))
+  expect_that(digest(x), equals("9c968dbb9a188795395b9cd66e87e27f"))
 
 })
