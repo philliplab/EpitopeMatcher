3273895217ebfb4963d3f4190946ef17e8da0a40
philliplab <philliplab@gmail.com>
2014-10-03 14:53:11 +0200

    add tests for the extra arguments of get_patient_id for AAStringSet

--- inst/tests/test-msa.R
+++ inst/tests/test-msa.R
@@ -1,18 +1,22 @@
 context("MSA")
 
 test_that("read_query_alignment works", {
-#  x <- read_query_alignment(file.path(find.package('EpitopeMatcher', .libPaths()), 
-#                                  'test_data/query_alignment_test_file.fasta'))
   x <- get_test_query_alignment()
   expect_that(x, is_a('AAStringSet'))
   expect_that(digest(x), equals("9beaf63ab683c0353b9dea853b13ef9b"))
 })
 
 test_that("get_patient_ids work", {
-#  x <- read_query_alignment(file.path(find.package('EpitopeMatcher', .libPaths()), 
-#                                  'test_data/query_alignment_test_file.fasta'))
   x <- get_test_query_alignment()
   ids <- get_patient_ids(x)
+  expect_that("hxb2" %in% ids, is_true())
+  expect_that("hxb2 " %in% ids, is_false())
+  expect_that("pat02" %in% ids, is_true())
+
+  x <- AAStringSet(structure(c( "MG", "MG", "MG", "MG"), 
+      .Names = c("17h00,hxb2 ,sdklfj", "45h34,pat01,scribbles", 
+                 ",pat02", "hello, pat03")))
+  ids <- get_patient_ids(x, ',', 2)
   expect_that("hxb2" %in% ids, is_true())
   expect_that("hxb2 " %in% ids, is_false())
   expect_that("pat02" %in% ids, is_true())
