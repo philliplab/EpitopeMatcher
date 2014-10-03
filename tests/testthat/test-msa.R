d65a38e7334b39da7aa375a18933021369474a21
philliplab <philliplab@gmail.com>
2014-10-03 09:53:36 +0200

    fix file names in query_alignment reader tests

--- tests/testthat/test-msa.R
+++ tests/testthat/test-msa.R
@@ -2,14 +2,14 @@ context("MSA")
 
 test_that("read_query_alignment works", {
   x <- read_query_alignment(file.path(find.package('EpitopeMatcher', .libPaths()), 
-                                  'test_data/query_alignment.FASTA'))
+                                  'test_data/query_alignment_test_file.fasta'))
   expect_that(x, is_a('AAStringSet'))
-  expect_that(digest(x), equals("b2a9316b11da1c07d56e5846e88ce49c"))
+  expect_that(digest(x), equals("9beaf63ab683c0353b9dea853b13ef9b"))
 })
 
 test_that("get_patient_ids work", {
   x <- read_query_alignment(file.path(find.package('EpitopeMatcher', .libPaths()), 
-                                  'test_data/query_alignment.FASTA'))
+                                  'test_data/query_alignment_test_file.fasta'))
   ids <- get_patient_ids(x)
   expect_that("P00885" %in% ids, is_true())
   expect_that("V05130" %in% ids, is_true())
