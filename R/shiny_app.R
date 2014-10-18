74ab88e548fa902010122ae7f48fe0960f1eff16
philliplab <philliplab@gmail.com>
2014-10-18 17:21:16 +0200

    add port option to run_EpitopeMatcher_app

--- R/shiny_app.R
+++ R/shiny_app.R
@@ -2,8 +2,12 @@
 #' 
 #' @export
 
-run_EpitopeMatcher_app <- function(){
+run_EpitopeMatcher_app <- function(port = NULL){
   packageDir <- find.package('EpitopeMatcher')
   shinyAppDir <- file.path(packageDir, "www") 
-  runApp(shinyAppDir)
+  if (is.null(port)){
+    runApp(shinyAppDir, host = "0.0.0.0")
+  } else {
+    runApp(shinyAppDir, port = port, host = "0.0.0.0")
+  }
 }
